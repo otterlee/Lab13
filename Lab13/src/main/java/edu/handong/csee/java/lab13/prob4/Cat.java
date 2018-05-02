af900c110ea8dc4b830243511f3d3d205bfcfb2a
otterlee <21700525@handong.edu>
2018-05-03 01:15:22 +0900

    Add comment in Dog class and Cat class

--- Lab13/src/main/java/edu/handong/csee/java/lab13/prob4/Cat.java
+++ Lab13/src/main/java/edu/handong/csee/java/lab13/prob4/Cat.java
@@ -1,6 +1,6 @@
 package edu.handong.csee.java.lab13.prob4;
 
-public class Cat extends Animal implements Pet{
+public class Cat extends Animal implements Pet{ //the subclass derived from class Animal and implement interface Pet
 	//class Cat is derived class from class Animal, and it implements interface Pet.
 	public void food() {//method 'food' to print-out message about cat's food.
 		System.out.println("Feed : Cat should eat fish!"); //print-out message about cat's food.
--- Lab13/src/main/java/edu/handong/csee/java/lab13/prob4/Dog.java
+++ Lab13/src/main/java/edu/handong/csee/java/lab13/prob4/Dog.java
@@ -1,6 +1,6 @@
 package edu.handong.csee.java.lab13.prob4;
 
-public class Dog extends Animal implements Pet{
+public class Dog extends Animal implements Pet{//the subclass derived from class Animal and implement interface Pet
 	//class Dog is derived class from class Animal, and it implements interface Pet.
 	public void food() {//method 'food' to print-out message about dog's food.
 		System.out.println("Feed : Dog should eat Sausage!");//print-out message about dog's food.
