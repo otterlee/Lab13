29efebc7379e84fb3a84feabaf65ff685ef1e8a6
otterlee <21700525@handong.edu>
2018-05-03 01:17:16 +0900

    Add comment in interface Pet

--- Lab13/src/main/java/edu/handong/csee/java/lab13/prob4/Pet.java
+++ Lab13/src/main/java/edu/handong/csee/java/lab13/prob4/Pet.java
@@ -1,6 +1,6 @@
 package edu.handong.csee.java.lab13.prob4;
 
-public interface Pet {
+public interface Pet { //interface that include methods showName and food.
 	//interface which performs important action using pet's name and pet's food.
 	public void showName();//use method 'showName' 
 	public void food();//use method 'food'
